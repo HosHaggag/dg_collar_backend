f996ff0e9cecbc05b51d9595a011a675cb48fa0c
mrsawy <mrsawy@gmail.com>
2024-05-16 08:51:30 +0300

    stable

--- controllers/data.js
+++ controllers/data.js
@@ -1,5 +1,5 @@
 const moment = require("moment-timezone");
-const Data = require("../models/Data");
+const Data = require("../models/data");
 
 const { addDataSchema, getDataSchema } = require("../validators/data");
 
